5f8e9b01e04bae69aec765d793e4218972f38de3
DStarovoitov <!QAZxsw2>
2022-05-05 17:17:26 +0300

    add login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
+import {Button, Container, Form} from "react-bootstrap";
+
+import "./index.css";
 
 export default function Login() {
+	const [login, setLogin] = useState("");
+	const [password, setPassword] = useState("");
+
+	function handleLogin () {
+		if (password === "12345" && login === "admin") {
+			localStorage.setItem("news_auth", "true");
+
+			history.push("/profile");
+		}
+	}
+
 	return (
-		<div>
-			LOGIN
-		</div>
+		<Container>
+			<Form className={"form"}>
+				<input
+					placeholder={"Username"}
+					value={login}
+					onChange={(event) => setLogin(event.target.value)}
+				/>
+
+				<input
+					placeholder={"Password"}
+					type={"password"}
+					value={password}
+					onChange={(event) => setPassword(event.target.value)}
+				/>
+
+				<Button onClick={() => handleLogin()}>
+					Log in
+				</Button>
+			</Form>
+		</Container>
 	);
 }
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import {Container} from "react-bootstrap";
+import {Button, Container} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function Main() {
 	return (
@@ -10,6 +11,16 @@ export default function Main() {
 			<div>
 				Go to look some <Link to={"/news"}>news</Link>
 			</div>
+
+			<div>
+				Please, log in for check this functional
+			</div>
+
+			<LinkContainer to={"/login"}>
+				<Button>
+					Log in
+				</Button>
+			</LinkContainer>
 		</Container>
 	);
 }
