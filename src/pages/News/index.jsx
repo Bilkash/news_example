b77e8cfda07cb5e84fae78988d6ad8009a18cb20
DStarovoitov <!QAZxsw2>
2022-05-08 12:28:24 +0300

    add search

--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {Container} from "react-bootstrap";
+import {Button, Container} from "react-bootstrap";
 import axios from "axios";
 import Article from "../../components/Article";
 
@@ -7,12 +7,13 @@ import "./index.css";
 
 export default function News() {
 	const [list, setList] = useState([]);
+	const [search, setSearch] = useState("");
 
-	const fetchData = async () => {
+	const fetchData = async (query) => {
 		const response = await axios.get("https://newsapi.org/v2/everything", {
 			params: {
 				sortBy: "popularity",
-				q: "Ukraine",
+				q: query ?? "Ukraine",
 				apiKey: "07e074fe4bb6498db01f6b345a2491a4"
 			}
 		}).then(res => res);
@@ -20,13 +21,35 @@ export default function News() {
 		setList(response.data.articles);
 	};
 
+	function handleSearch() {
+		fetchData(search);
+	}
+
 	useEffect( () => {
 		fetchData();
 	}, []);
 
+	useEffect(() => {
+		if (!search) {
+			fetchData();
+		}
+	}, [search]);
+
 	return (
 		<Container>
 			<h1>NEWS</h1>
+
+			<div className={"search"}>
+				<input
+					placeholder={"Search news"}
+					value={search}
+					onChange={(event) => setSearch(event.target.value)}
+				/>
+
+				<Button onClick={() => handleSearch()}>
+					Search
+				</Button>
+			</div>
 
 			<div className={"list"}>
 				{list.map((it) => {
